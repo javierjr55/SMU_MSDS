42099fe7eb3a24a150464fc4423853f817ba3d57
Javier Saldaña Jr <javierjr55@hotmail.com>
2018-09-13 22:05:31 -0500

    Added files to Function_Codes directory
    Directory holds templates of function codes in order to facilitate future use.
    R_Code.r
    SAS_Code.sas

---
+++ Function_Codes/R_Code.r
@@ -0,0 +1,8 @@
+#R Code used for school
+#QQ Plot function
+#Use the qqnorm() function in order to take the QQ of the sample group. 
+#Sample code below:
+qqnorm(datasource$variable[datasource$class==class_name], main = "title")
+qqplot(datasource$variable[datasource$class==class_name])
+
+#The code above would generate the 
